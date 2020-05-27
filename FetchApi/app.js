a6143fe974e78bf2113d447791d8d7709233ae9a
Kaushal Mehra <kaushal.mehra@walmart.com>
2020-05-28 03:20:14 +0530

    fetch api changes

--- FetchApi/app.js
+++ FetchApi/app.js
@@ -1,7 +1,10 @@
+let textarea = document.querySelector('#fetchData');
+
 function loadData() {
   fetch("https://jsonplaceholder.typicode.com/users", {})
     .then(response => {
       console.log(response);
+      textarea.innerHTML= JSON.stringify(response);
     })
     .then(data => {
       console.log(data);
