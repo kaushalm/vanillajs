5e212dd3f78318032737262174bcaaac2a569878
Kaushal Mehra <kaushalmehra1987@gmail.com>
2020-06-03 02:44:50 +0530

    custom calander view

--- Calander/app.js
+++ Calander/app.js
@@ -1,7 +1,10 @@
 let textarea = document.querySelector('#fetchData');
 
 function loadData() {
-  fetch("https://jsonplaceholder.typicode.com/users", {})
+  //const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  fetch("https://hn.algolia.com/api/v1/search?query=git",{
+    //mode: 'no-cors', // no-cors, *cors, same-origin
+  })
     .then(response => {
       console.log(response);
       textarea.innerHTML= JSON.stringify(response);
