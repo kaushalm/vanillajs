91116c93ce0ffda24330766dfa520bc76349809d
Kaushal Mehra <kaushalmehra1987@gmail.com>
2021-01-25 22:27:27 +0530

    file reader and main page logic

--- DragFiles/index.js
+++ DragFiles/index.js
@@ -1,41 +1,40 @@
 let dropzone = document.querySelector("#dropzone");
 let fileUpload = document.querySelector("#fileUpload");
-
+let imagepreview = document.querySelector("#imagepreview");
 
 dropzone.addEventListener("dragleave", (e) => {
   e.preventDefault();
   dropzone.className = "dropzone";
-  console.log("dragleave");
 });
 
 dropzone.addEventListener("dragover", (e) => {
   e.preventDefault();
   dropzone.className = "dragover";
-  console.log("dragover");
 });
 
 dropzone.addEventListener("drop", (e) => {
   e.preventDefault();
-  console.log("drop");
 
-  var reader = new FileReader();
-    reader.readAsDataURL(e.dataTransfer.files[0]);
-    reader.onload = function(readEvent) {
-    document.querySelector("#image").append("<img src='"+readEvent.target.result+"' />");
-    }
+  //add a file reder to read contents of file
+  const reader = new FileReader();
+  reader.onload = function (readEvent) {
+    imagepreview.setAttribute("src", readEvent.target.result);
+  };
+  reader.readAsDataURL(e.dataTransfer.files[0]);
 });
 
 dropzone.addEventListener("click", (e) => {
   console.log("click");
 });
 
-
 fileUpload.addEventListener("change", (e) => {
-    e.preventDefault();
-    var reader = new FileReader();
-    reader.readAsDataURL(e.currentTarget.files[0]);
-    reader.onload = function(readEvent) {
-    document.querySelector("#image").append("<img src='"+readEvent.target.result+"' />");
-    }
-    console.log("file Upload");
-  });
+  e.preventDefault();
+  //add a file reder to read contents of file
+
+  const reader = new FileReader();
+  reader.onload = function (readEvent) {
+    imagepreview.setAttribute("src", readEvent.target.result);
+  };
+
+  reader.readAsDataURL(e.currentTarget.files[0]);
+});
